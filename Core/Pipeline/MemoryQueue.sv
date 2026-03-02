e496ad8a85f6b7db1bf752875f1bd82264f78e41
John <johnchyatt@icloud.com>
2026-03-02 14:31:08 -0600

    Prototype 2.5

--- Core/Pipeline/MemoryQueue.sv
+++ Core/Pipeline/MemoryQueue.sv
@@ -1,3 +1,12 @@
 import Configuration::*;
 import Payloads::*;
 import Enumerations::*;
+
+// mini single req store buffer with forwarding so you dont need to wait for ack
+// back. fairly simple, just hold recent outgoing store and forward
+// to in flight loads. This is past ROB and lets stores commit fast.
+// basically stall mem queue but forward to loads if needed. must keep
+// bus live to maintain transaction for ack.
+// actually with inter queue forwarding this may not be necissary.
+// unless theres a case im missing, rob doesnt need ack to commit stores
+// so longas the bus transaction holds until ack. 
